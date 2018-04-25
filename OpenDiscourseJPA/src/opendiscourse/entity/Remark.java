32e50974ac69af5c7ac6da72af1ab6a3fb5106b6
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2018-04-25 18:26:39 -0400

    generate jpa entities

---
+++ OpenDiscourseJPA/src/opendiscourse/entity/Remark.java
@@ -0,0 +1,73 @@
+package opendiscourse.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the remark database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Remark.findAll", query="SELECT r FROM Remark r")
+public class Remark implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int idremark;
+
+	private String remarkValue;
+
+	private int thumbsDown;
+
+	private int thumbsUp;
+
+	//bi-directional many-to-one association to Topic
+	@ManyToOne
+	@JoinColumn(name="idTopic")
+	private Topic topic;
+
+	public Remark() {
+	}
+
+	public int getIdremark() {
+		return this.idremark;
+	}
+
+	public void setIdremark(int idremark) {
+		this.idremark = idremark;
+	}
+
+	public String getRemarkValue() {
+		return this.remarkValue;
+	}
+
+	public void setRemarkValue(String remarkValue) {
+		this.remarkValue = remarkValue;
+	}
+
+	public int getThumbsDown() {
+		return this.thumbsDown;
+	}
+
+	public void setThumbsDown(int thumbsDown) {
+		this.thumbsDown = thumbsDown;
+	}
+
+	public int getThumbsUp() {
+		return this.thumbsUp;
+	}
+
+	public void setThumbsUp(int thumbsUp) {
+		this.thumbsUp = thumbsUp;
+	}
+
+	public Topic getTopic() {
+		return this.topic;
+	}
+
+	public void setTopic(Topic topic) {
+		this.topic = topic;
+	}
+
+}
---
+++ OpenDiscourseJPA/src/opendiscourse/entity/Topic.java
@@ -0,0 +1,67 @@
+package opendiscourse.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the topic database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Topic.findAll", query="SELECT t FROM Topic t")
+public class Topic implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int idTopic;
+
+	private String topicValue;
+
+	//bi-directional many-to-one association to Remark
+	@OneToMany(mappedBy="topic")
+	private List<Remark> remarks;
+
+	public Topic() {
+	}
+
+	public int getIdTopic() {
+		return this.idTopic;
+	}
+
+	public void setIdTopic(int idTopic) {
+		this.idTopic = idTopic;
+	}
+
+	public String getTopicValue() {
+		return this.topicValue;
+	}
+
+	public void setTopicValue(String topicValue) {
+		this.topicValue = topicValue;
+	}
+
+	public List<Remark> getRemarks() {
+		return this.remarks;
+	}
+
+	public void setRemarks(List<Remark> remarks) {
+		this.remarks = remarks;
+	}
+
+	public Remark addRemark(Remark remark) {
+		getRemarks().add(remark);
+		remark.setTopic(this);
+
+		return remark;
+	}
+
+	public Remark removeRemark(Remark remark) {
+		getRemarks().remove(remark);
+		remark.setTopic(null);
+
+		return remark;
+	}
+
+}
