a084d397c80b419d6afebb40e61a47ba8a2be708
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2018-04-25 18:29:40 -0400

    remove filthy EJB project

--- OpenDiscourseEJB/ejbModule/opendiscourse/bean/RemarkRemote.java
+++
@@ -1,8 +0,0 @@
-package opendiscourse.bean;
-
-import javax.ejb.Remote;
-
-@Remote
-public interface RemarkRemote {
-
-}
--- OpenDiscourseEJB/ejbModule/opendiscourse/bean/TopicLocal.java
+++
@@ -1,8 +0,0 @@
-package opendiscourse.bean;
-
-import javax.ejb.Local;
-
-@Local
-public interface TopicLocal {
-
-}
